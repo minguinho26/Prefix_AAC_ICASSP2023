dee6e468567731f6266e8b34d3dab6b9bf51738b
Minkyu Kim <minkyu4506@gmail.com>
2022-08-31 23:57:29 +0900

    update

--- Clotho/Clotho_Dataset.py
+++ Clotho/Clotho_Dataset.py
@@ -14,15 +14,19 @@ class tokenizer_Clotho() :
     
     def encode(self, sentence) :
         
-        if self.vocab_size == 7556 :
+        if self.vocab_size == 7025 :
             sentence += '.'
-        
+        elif self.vocab_size == 4368 :
+            sentence = re.sub(r'\s([,.!?;:"](?:\s|$))', r'\1', sentence).replace('  ', ' ')
+            sentence = re.sub('[,.!?;:\"]', ' ', sentence).replace('  ', ' ')
+            sentence = sentence.strip()
+            
         word_list = sentence.split(' ')
         
         token_idx = []
         for word in word_list : 
             temp_word = word.lower()
-            if self.vocab_size == 4372 and temp_word[-1] == ',' :
+            if self.vocab_size == 4373 and temp_word[-1] == ',' :
                 temp_word_wo_rest = temp_word[:-1]
                 token_idx.append(self.vocab.index(temp_word_wo_rest))
                 token_idx.append(self.vocab.index(','))
@@ -55,10 +59,12 @@ class tokenizer_Clotho() :
         file_path = ''
         self.vocab_size = vocab_size
         
-        if vocab_size == 4372 :
-            file_path = './Clotho/Clotho_vocabulary_4372.pickle'
-        elif vocab_size == 7556 :
-            file_path = './Clotho/Clotho_vocabulary_7556.pickle'
+        if vocab_size == 4373 :
+            file_path = './Clotho/Clotho_vocabulary_4373.pickle'
+        elif vocab_size == 7025 :
+            file_path = './Clotho/Clotho_vocabulary_7025.pickle'
+        elif vocab_size == 4368 :
+            file_path = './Clotho/Clotho_vocabulary_4368.pickle'
         
         with open(file_path, 'rb') as f:
             self.vocab = pickle.load(f) 
@@ -71,7 +77,8 @@ class ClothoDataset(Dataset):
         
         self.change_sampling_rate = torchaudio.transforms.Resample(self.SAMPLE_RATE, 16000)
         
-        
+        self.split = split
+
         self.audio_files_dir = data_dir + '/clotho_audio_files/' + split
         
         csv_file_path = data_dir + '/clotho_csv_files/' + 'clotho_captions_' + split + '.csv'
@@ -80,7 +87,9 @@ class ClothoDataset(Dataset):
         
         self.audio_name_list = []
         self.token_list = []
+        self.caption_list_for_test = []
         all_audio_token_list = []
+        
         
         csv_file = pd.read_csv(csv_file_path)
         # audio의 경로, audio에 해당하는 caption을 리스트에 추가
@@ -89,11 +98,13 @@ class ClothoDataset(Dataset):
             self.audio_name_list.append(file)
             
             token_list_inEachAudio = []
+            caption_list_each_audio_for_test = []
             for i in range(5) :
                 
                 sentence_str = 'caption_' + str(i + 1)
                 caption = csv_file[csv_file['file_name'] == file][sentence_str].item()
 
+                # 문장 교정================================
                 # 마침표 제거
                 caption = re.sub(r'[.]', '', caption) 
 
@@ -104,26 +115,43 @@ class ClothoDataset(Dataset):
                 caption = re.sub(' +', ' ', caption)
 
                 caption = caption.replace(' ,', ',')
-                
+                    
                 # caption의 마지막이 쉼표일 경우 제거
                 if caption[-1] == ',' :
                     caption = caption[:-1]
-                    
-                caption = caption.strip()  
+
+                caption = caption.strip()
+                # 문장 교정================================
                 
-                if tokenizer_type == 'GPT2' :
-                    caption += '.'
-                    tokens = tokenizer(caption)['input_ids']
+                if split == 'development' :
+                
+                    if tokenizer_type == 'GPT2' :
+                        if caption[-1] != '.' :
+                            caption += '.'
+                        tokens = tokenizer(caption)['input_ids']
+                    else :
+                        tokens = tokenizer.encode(caption)
+
+                    token_list_inEachAudio.append(tokens)
+                    all_audio_token_list.append(tokens)
                 else :
-                    tokens = tokenizer.encode(caption)
-                
-                token_list_inEachAudio.append(tokens)
-                all_audio_token_list.append(tokens)
+                    if tokenizer.vocab_size == 4368 :
+                        caption = re.sub(r'\s([,.!?;:"](?:\s|$))', r'\1', caption).replace('  ', ' ')
+                        caption = re.sub('[,.!?;:\"]', ' ', caption).replace('  ', ' ')
+                        caption = caption.strip()
+                    elif tokenizer.vocab_size == 7025 :
+                        caption += '.'
+                        
+                    caption_list_each_audio_for_test.append(caption)
             
-            self.token_list.append(token_list_inEachAudio)     
-                                   
-        self.all_len = torch.tensor([len(all_audio_token_list[i]) for i in range(len(all_audio_token_list))]).float()
-        self.max_seq_len = min(int(self.all_len.mean() + self.all_len.std() * 10), int(self.all_len.max()))
+            if split == 'development' :
+                self.token_list.append(token_list_inEachAudio)  
+            else :
+                self.caption_list_for_test.append(caption_list_each_audio_for_test)
+                
+        if split == 'development' :                           
+            self.all_len = torch.tensor([len(all_audio_token_list[i]) for i in range(len(all_audio_token_list))]).float()
+            self.max_seq_len = min(int(self.all_len.mean() + self.all_len.std() * 10), int(self.all_len.max()))
         self.prefix_length = prefix_size
             
     def __len__(self):
@@ -166,26 +194,26 @@ class ClothoDataset(Dataset):
         audio_file_full_path = self.audio_files_dir + '/' + self.audio_name_list[item]
        
         audio_file, _ = torchaudio.load(audio_file_full_path)
-        audio_file = self.change_sampling_rate(audio_file).squeeze(0)
     
         # 지정한 Length를 기준으로 slicing or padding을 수행
         set_length = 30
         
-        change_sample_rate = 16000
+        sample_rate = 16000
         
         # slicing
-        if audio_file.shape[0] > (change_sample_rate * set_length) :
-            audio_file = audio_file[:change_sample_rate * set_length]
+        if audio_file.shape[0] > (sample_rate * set_length) :
+            audio_file = audio_file[:sample_rate * set_length]
         # zero padding
-        if audio_file.shape[0] < (change_sample_rate * set_length) :
-            pad_len = (change_sample_rate * set_length) - audio_file.shape[0]
+        if audio_file.shape[0] < (sample_rate * set_length) :
+            pad_len = (sample_rate * set_length) - audio_file.shape[0]
             pad_val = torch.zeros(pad_len)
             audio_file = torch.cat((audio_file, pad_val), dim=0)
-            
-        tokens, mask = self.pad_tokens(item)
         
-        # raw audio, gpt2_caption, file_name 출력
-        return audio_file, tokens.type(torch.int64), mask.type(torch.int64), self.audio_name_list[item]
+        if self.split == 'development' :
+            tokens, mask = self.pad_tokens(item)
+            return audio_file, tokens.type(torch.int64), mask.type(torch.int64), self.audio_name_list[item]
+        else :
+            return audio_file, self.caption_list_for_test[item], self.audio_name_list[item]
     
 
 def dataloader_ClothoDataset(tokenizer, data_dir, batch_size, split, prefix_size, is_TrainDataset = False) :
